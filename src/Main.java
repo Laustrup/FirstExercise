2488fbf26a56beb84da160362ed7102ad409960b
Laust <laust.bonnesen@mail.com>
2021-02-04 16:10:03 +0100

    Med Main

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,39 @@
+import java.util.Scanner;
+
 public class Main {
 
-    //Something
+    public static void main(String[] args) {
+
+        Kvadrat kvadrat = new Kvadrat();
+        Bog bog = new Bog();
+        Arrlist arrlist = new Arrlist();
+        Scanner scan = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Press 1 for Kvadrat, 2 for bog or 3 for arraylist:");
+            String typed = scan.nextLine();
+
+            switch (typed) {
+                case "1":
+                    System.out.println(kvadrat.getDescription());
+                kvadrat.assignment();
+                break;
+                case "2":
+                    System.out.println(bog.getDescription());
+                    bog.assignment();
+                    break;
+                case "3":
+                    System.out.println(arrlist.getDescription());
+                    arrlist.assignment();
+                    break;
+                default:
+                    System.out.println("Try again");
+            }
+            }
+
+        }
+        //bog, arraylist
+
+    }
+
 }
